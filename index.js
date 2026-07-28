3d7c34e650321bd7099aace4c7f588f14d97cc50
Juan San Emeterio <juanjser@gmail.com>
2026-07-28 19:46:13 -0400

    Clear stale event dates on form load

--- index.js
+++ index.js
@@ -227,6 +227,7 @@ function bindEstimateForm() {
       };
 
   if (dateInput) {
+    dateInput.value = '';
     const today = new Date();
     const month = String(today.getMonth() + 1).padStart(2, '0');
     const day = String(today.getDate()).padStart(2, '0');
